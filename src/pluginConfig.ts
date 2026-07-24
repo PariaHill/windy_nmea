792da71679fb758afcc722882b87182e466850c6
Park Kusang <fakearia@naver.com>
2026-07-24 09:13:32 +0900

    Use bundled icon for bridge executable

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-nmea',
-    version: '0.1.2',
+    version: '0.1.3',
     icon: '📍',
     title: 'NMEA Markers',
     description: 'Display NMEA position markers on Windy.',
